a26649e855c669a8971836d7ff123b5e1a1b18c1
Joel Alejandro Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2017-02-27 13:32:43 -0300

    added score component

---
+++ src/scene/score.js
@@ -0,0 +1,23 @@
+export default class Score {
+  constructor() {
+    this.__element__ = window.document.createElement('score');
+    this.__element__.controllerObject = this;
+    this.__meta__ = { score: 0 };
+    document.body.appendChild(this.__element__);
+  }
+
+  increaseBy(points) {
+    this.__meta__.score += points;
+    this.refreshScoreUI();
+  }
+
+  decreaseBy(points) {
+    this.__meta__.score -= points;
+    this.refreshScoreUI();
+  }
+
+  refreshScoreUI() {
+    const score = this.__meta__.score.toString();
+    this.__element__.textContent = "0".repeat(8).substr(0, 8 - score.length) + score;
+  }
+}
