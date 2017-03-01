b000ae03419feeab253cbb807b6d3bb9f05a25ee
Joel Alejandro Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2017-03-01 01:34:00 -0300

    added PointerLock-based movement support

--- src/ship/player-ship.js
+++ src/ship/player-ship.js
@@ -2,14 +2,25 @@ import Ship from './ship';
 import EnergyCannon from '../weapons/energy-cannon';
 
 export default class PlayerShip extends Ship {
-  constructor(name) {
-    super('PLAYER_SHIP', name);
+  constructor(options) {
+    super({
+      id: 'PLAYER_SHIP',
+      name: options.name,
+      shipClass: options.shipClass
+    });
     this.__isPlayerShip__ = true;
   }
 
-  move(event) {
-    this.__element__.style.left = `${event.x + (this.__meta__.shipSize.width / 2)}px`;
-    this.__element__.style.top = `${event.y + (this.__meta__.shipSize.height / 2)}px`;
+  detectMovements() {
+    const scene = this.__element__.ownerDocument.querySelector('scene').controllerObject;
+    const pointer = scene.getPointerData();
+    if (pointer && pointer.relative) {
+      super.move(pointer.relative, scene);
+    }
+    // FIXME: This breaks movement once focus is lost.
+    if (scene.canPointerMove) {
+      this.dispatchMovementDetection();
+    }
   }
 
   renderTo(scene) {
@@ -17,11 +28,21 @@ export default class PlayerShip extends Ship {
 
     this.__element__.setAttribute('controlled-by', 'player');
 
-    scene.addEventListener('mousemove', this.move.bind(this), false);
+    // scene.addEventListener('mousemove', this.move.bind(this), false);
+    this.dispatchMovementDetection();
     this.mountWeapon();
   }
 
+  dispatchMovementDetection() {
+    this.__meta__.previousPosition = this.getPosition();
+    this.__meta__.movementDetectorID = window.requestAnimationFrame(this.detectMovements.bind(this));
+  }
+
   mountWeapon() {
-    this.__meta__.weapon = new EnergyCannon(3, this);
+    this.__meta__.weapon = new EnergyCannon({
+      power: 3,
+      owner: this,
+      energy: 'plasma'
+    });
   }
 }
